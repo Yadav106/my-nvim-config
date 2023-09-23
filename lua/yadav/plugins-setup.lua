093ea4a2bbe63e8303f42e5169829fa5028fd445
Yadav106 <sahil.jp.yadav@gmail.com>
2023-09-23 21:58:54 +0530

    added treesitter, formatter and linter

--- lua/yadav/plugins-setup.lua
+++ lua/yadav/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 local packer_bootstrap = ensure_packer()
 
@@ -20,70 +20,84 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+	return
 end
 
 return packer.startup(function(use)
-  use("wbthomason/packer.nvim")
-  use("bluz71/vim-nightfly-guicolors") --preferred colorscheme
-  -- tmux & split window config
-  use("christoomey/vim-tmux-navigator")
-  use("szw/vim-maximizer") -- maximizes and restores current window
+	use("wbthomason/packer.nvim")
+	use("bluz71/vim-nightfly-guicolors") --preferred colorscheme
+	-- tmux & split window config
+	use("christoomey/vim-tmux-navigator")
+	use("szw/vim-maximizer") -- maximizes and restores current window
 
-  -- lua functions that many plugins use
-  use("nvim-lua/plenary.nvim")
+	-- lua functions that many plugins use
+	use("nvim-lua/plenary.nvim")
 
-  -- essential plugins
-  use("tpope/vim-surround")
-  use("vim-scripts/ReplaceWithRegister")
+	-- essential plugins
+	use("tpope/vim-surround")
+	use("vim-scripts/ReplaceWithRegister")
 
-  -- commenting with gc
-  use("numToStr/Comment.nvim")
+	-- commenting with gc
+	use("numToStr/Comment.nvim")
 
-  -- file explorer
-  use("nvim-tree/nvim-tree.lua")
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
 
-  -- icons for nvim-tree
-  use("kyazdani42/nvim-web-devicons")
+	-- icons for nvim-tree
+	use("kyazdani42/nvim-web-devicons")
 
-  -- statusline
-  use("nvim-lualine/lualine.nvim")
+	-- statusline
+	use("nvim-lualine/lualine.nvim")
 
-  -- telescope fuzzy finder
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+	-- telescope fuzzy finder
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
-  -- autocompletion
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
+	-- autocompletion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
 
-  -- snippets
-  use("L3MON4D3/LuaSnip")
-  use("saadparwaiz1/cmp_luasnip")
-  use("rafamadriz/friendly-snippets")
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
 
- -- managing & installing lsp servers
- use("williamboman/mason.nvim")
- use("williamboman/mason-lspconfig.nvim")
+	-- managing & installing lsp servers
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
 
- -- configuring lsp servers
- use("neovim/nvim-lspconfig")
- use("hrsh7th/cmp-nvim-lsp")
- use({
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    requires = {
-      { "nvim-tree/nvim-web-devicons" },
-      { "nvim-treesitter/nvim-treesitter" },
-    },
-  }) -- enhanced lsp uis
-  use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	-- linting & formatting
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
 
-  if packer_bootstrap then
-    require("packer").sync()
-  end
-end
-)
+	-- treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
 
+	-- auto closing
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use({
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+		requires = {
+			{ "nvim-tree/nvim-web-devicons" },
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	}) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
+end)
